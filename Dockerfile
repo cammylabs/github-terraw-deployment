75f85bc9347cb5a00673b908d84733914f4d62d3
Oskar <oskar@cammy.com>
2019-11-27 13:34:55 +1100

    Version 1.0

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM docker:dind
+MAINTAINER Cammy <oskar@cammy.com>
+
+RUN apk -v --update add \
+        python \
+        py-pip \
+        groff \
+        less \
+        mailcap \
+        && \
+    pip install --upgrade awscli==1.14.5 s3cmd==2.0.1 python-magic && \
+    apk -v --purge del py-pip && \
+    rm /var/cache/apk/*
+
+ADD /entrypoint.sh /entrypoint.sh
+RUN ["chmod", "+x", "/entrypoint.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+function main(){
+  sanitize "${INPUT_ENVIROMENT}" "enviroment"
+  sanitize "${INPUT_AWS_REGION}" "aws_region"
+  sanitize "${INPUT_ACCESS_KEY_ID}" "access_key_id"
+  sanitize "${INPUT_SECRET_ACCESS_KEY}" "secret_access_key"
+
+  setUpAWSCredential INPUT_ENVIROMENT
+  runDeployer
+}
+
+function sanitize(){
+ echo ${1} "---" ${2}
+  if [ -z "${1}" ]; then
+    >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
+    exit 1
+  fi
+}
+
+function  setUpAWSCredential() {
+  echo 'SETTING UP AWS CREDENTIAL FOR ' ${INPUT_ENVIROMENT}
+
+  export AWS_ACCESS_KEY_ID=${INPUT_ACCESS_KEY_ID}
+  export AWS_SECRET_ACCESS_KEY=${INPUT_SECRET_ACCESS_KEY}
+  export AWS_DEFAULT_REGION=${INPUT_AWS_REGION}
+
+  aws configure set aws_access_key_id ${INPUT_ACCESS_KEY_ID}
+  aws configure set aws_secret_access_key ${INPUT_SECRET_ACCESS_KEY}
+  aws configure set region ${INPUT_AWS_REGION}
+
+  echo 'AWS CREDENTIALS DONE'
+}
+
+function runDeployer() {
+  echo 'RUNINING DEPLOYMENT FOR ' ${INPUT_ENVIROMENT}
+  cd deployment
+  sh ./deploy.sh ${INPUT_ENVIROMENT}
+
+  echo 'DEPLOYMENT ' ${INPUT_ENVIROMENT} ' DONE'
+}
+
+echo '[entrypoint.sh] STARING DEPLOYMENT'
+main
